ae2ece7232526064992479bfe7f00e1c2ead4d0e
Caroline Kish <ckish3@gatech.edu>
2019-03-31 09:47:05 -0400

    added checkoff_sheet structure example to docs folder and completed initial checkoff_sheets collection functionality

--- db_handler.py
+++ db_handler.py
@@ -6,36 +6,70 @@ connection = None
 db = None
 
 def connect():
-    """connect to mongodb's orgt database and initialize global 'connection' variable"""
+    """
+    connect to mongodb's orgt database and initialize global 'connection' variable
+    """
     global connection
     global db
     connection = MongoClient()
     db = connection.orgt_db
 
 def disconnect():
-    """disconnet from mongodb's orgt database"""
+    """
+    disconnet from mongodb's orgt database
+    """
     global connection
     connection.close()
 
-def insert_document_checkoff_sheets_collection_(doc):
+def insert_document_checkoff_sheets_collection(doc):
+    """
+    insert document into checkoff_sheets collection
+    """
     try:
         connect()
-        db.checkoff_sheets.insert(doc)
+        if type(doc) == list:
+            db.checkoff_sheets.insert_many(doc)
+        else:
+            db.checkoff_sheets.insert_one(doc)
         disconnect()
     except Exception as ex:
-        print("insert_document_checkoff_sheets_collection_ error: {}".format(ex.getMessage()))
+        print("insert_document_checkoff_sheets_collection_ error: {}".format(ex))
 
 def select_document_checkoff_sheets_collection(id=None):
     try:
         connect()
         if id is None:
+            # result is an 'iterator' of dict obj
+            # each dict obj is a document and can be treated
+            # like a regular python dict
             result = db.checkoff_sheets.find()
         else:
             result = db.checkoff_sheets.find({"_id": id})
         disconnect()
+        return result
     except Exception as ex:
-        print("select_document_checkoff_sheets_collection error: {}".format(ex.getMessage()))
+        print("select_document_checkoff_sheets_collection error: {}".format(ex))
 
-# debugging 
+def delete_document_checkoff_sheets_collection(id=None):
+    try:
+        connect()
+        result = db.checkoff_sheets.delete_one({"_id": id})
+        # result.deleted_count -> # of deleted documents
+        # result.acknowledged -> True if operation with write concern and false if write concern was disabeled
+        disconnect()
+        return result
+    except Exception as ex:
+        print("delete_document_checkoff_sheets_collection error: {}".format(ex))
+
+def insert_document_staff_members_collection():
+    pass 
+
+# debugging
 if __name__=="__main__":
-    pass
+    print()
+    # insert_document_checkoff_sheets_collection({"_id": 4})
+    delete_document_checkoff_sheets_collection(2)
+    result = select_document_checkoff_sheets_collection()
+    for doc in result:
+        print(doc["_id"])
+    print()
