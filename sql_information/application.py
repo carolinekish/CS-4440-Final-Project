614bbc37831fd08135126e4ec69b3ece6e38bf0a
Michael Sherman <msherman32@gatech.edu>
2019-04-15 21:03:10 -0400

    logged in user can only view his own and can sign of on others

--- sql_information/application.py
+++ sql_information/application.py
@@ -3,13 +3,17 @@ from flask import Flask, redirect, url_for, request, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import  String, Column, VARCHAR
 from sqlalchemy.sql import func
-# from database import db_session
 application = Flask(__name__)
 # DB name = orgt_chechoffs
 # todo: shouldn't a checkoff belong to a sport or a checkoff sheet?
 
-orgt_member = ""
+# THIS IS THE LOGGED IN USER:
+member_name = ""
 member_id = 0 # 0 is not a valid member_id
+
+# THIS IS THE STAFF MEMBER THE LOGGED IN USER SEARCHED FOR:
+searched_member_name = ""
+searched_member_id = 0
 
 application.secret_key = "super secret key"
 application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://orgt4440:finalproject4440@orgt4440.ccfdnjnpnqku.us-east-2.rds.amazonaws.com:3306/orgt_chechoffs'
@@ -38,10 +42,14 @@ def start():
 def login():
    if request.method == 'POST':
       orgt_member = request.form['member_name']
-      member_id_query = db.engine.execute('SELECT member_id FROM staff_members WHERE name=%s', orgt_member)
-      global member_id # needed in order to MODIFY a global variable
-      member_id = member_id_query.fetchall()[0].member_id # should we add them to the table if they are not already added
-      return redirect(url_for('choose_sport', name = orgt_member))
+      member_id_query = db.engine.execute('SELECT * FROM staff_members WHERE name=%s', orgt_member)
+      result = member_id_query.first()
+      if result != [] :
+         global member_id # needed in order to MODIFY a global variable
+         global member_name
+         member_id = result[0] # should we add them to the table if they are not already added
+         member_name = result[1]
+         return redirect(url_for('choose_sport', name = member_name))
    else:
       # request.method == 'GET'
       # orgt_member = request.args.get('member_name')
@@ -53,8 +61,24 @@ def choose_sport(name):
    orgt_sports = sports_query.fetchall()
    return render_template('sports.html', name=name, sports=orgt_sports)
 
-@application.route('/checkoff_sheet/<name>', methods = ['POST'])
-def display_checkoff_sheet(name):
+@application.route('/checkoff_sheet_user', methods=["POST"])
+def show_own_checkoffs():
+   return redirect(url_for('display_checkoff_sheet', name = member_name, member_id = member_id))
+
+@application.route("/search_staff", methods=["POST"])
+def search_staff():
+   search_name = request.form["search_name"]
+   query = db.engine.execute('SELECT * FROM staff_members WHERE name=%s',search_name)
+   found_member = query.first()
+   if found_member != [] :
+      global searched_member_id
+      global searched_member_name
+      searched_member_id = found_member[0]
+      searched_member_name = found_member[1]
+      return redirect(url_for('display_checkoff_sheet', name = searched_member_name, member_id = searched_member_id))
+    
+@application.route('/checkoff_sheet/<name>_<member_id>') #, methods = ['POST']
+def display_checkoff_sheet(name, member_id):
    startTime = time.time() # start time
 
    posQuery = db.engine.execute('SELECT position_name FROM sheet_contains WHERE sheet_name = \'Rock Climbing Checkoff Sheet\'') # todo: paramaterize the sport?
@@ -75,7 +99,13 @@ def display_checkoff_sheet(name):
    print completed_ids
    reqs = reqsQuery.fetchall()
    
-   return render_template('checkoffs.html', 
+   view = ''
+   if name==member_name and member_id==member_id :
+      view = 'checkoffs_view_only.html'
+   else:
+      view = 'checkoffs.html'
+   
+   return render_template(view, 
          name = name,
          categories=cat,
          positions=positions,
@@ -85,54 +115,16 @@ def display_checkoff_sheet(name):
          requirements=reqs,
          time=query_execution_time) 
 
-# instead of this route, just post back to the other method
 @application.route('/checkoff_sheet_edit/<checkoff_id>', methods = ['POST'])
 def display_checkoff_sheet_edit(checkoff_id):  
-   # find out how to pass name through here
-   # todo: find out how to not route if 
       if request.method == 'POST':
-         name_member_signing_off = request.form['signing_member_name']
-         if (not name_member_signing_off):
-            flash("Signature field is empty")
-         else:
-            print member_id
-            print checkoff_id 
-            print name_member_signing_off
-            db.engine.execute('INSERT INTO completed_checkoffs (member, checkoff, signature, date_time) VALUES (%s,%s,%s,CURRENT_TIMESTAMP)', member_id, checkoff_id, name_member_signing_off)
-         # display_checkoff_sheet(name)
-      return 
-
-      # return display_checkoff_sheet(orgt_member) 
-      # todo: this does NOT change the route name
-
-      # return redirect(url_for('choose_sport', name = orgt_member))
-
-      # startTime = time.time()
-      # posQuery = db.engine.execute('SELECT * FROM position WHERE abbreviation = \'IIT\'')
-      # categoryQuery = db.engine.execute('SELECT * FROM category WHERE name = \'Competency and Personal Checkoffs\'')
-      # checkoffQuery = db.engine.execute('SELECT * FROM checkoff')
-      # reqsQuery = db.engine.execute('SELECT * FROM requirement ')
-      # endTime = time.time()
-
-      # query_execution_time = endTime - startTime
-      # pos = posQuery.fetchall()
-      # cat = categoryQuery.fetchall()
-      # chks = checkoffQuery.fetchall()
-      # reqs = reqsQuery.fetchall()
-      # return render_template('checkoffs.html', 
-      #       name = name,
-      #       categories=cat,
-      #       positions=pos,
-      #       checkoffs=chks,
-      #       requirements=reqs,
-      #       time=query_execution_time) 
-
-
-
+         db.engine.execute('INSERT INTO completed_checkoffs (member, checkoff, signature, date_time) VALUES (%s,%s,%s,CURRENT_TIMESTAMP)', searched_member_id, checkoff_id, member_name)
+         return redirect(url_for('display_checkoff_sheet', name = searched_member_name, member_id = searched_member_id))
 
 @application.route('/success/<name><dumb_var>')
 def success(name, dumb_var):
    return 'welcome %s, %s' % (name, dumb_var)
+
 if __name__ == '__main__':
     application.run(debug=True, use_reloader=True)      
 
